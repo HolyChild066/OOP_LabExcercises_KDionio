fd579bbc3fc81ae35bdc87a946ba3e9d89c5fb3f
HolyChild <kinlee.dionio@dorsu.edu.ph>
2025-09-25 09:42:49 +0800

    Update Person.java

--- EXER3_DIONIO/EXER_B/Person.java
+++ EXER3_DIONIO/EXER_B/Person.java
@@ -1,6 +1,6 @@
 public class Person {
-    private String name;
-    private int age;
+    protected String name;
+    protected int age;
 
     public Person() {
         this.name = "";
@@ -10,6 +10,12 @@ public class Person {
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    // Method overloading - same name, different parameters
+    public Person(String name) {
+        this.name = name;
+        this.age = 0;
     }
 
     public String getName() {
@@ -26,5 +32,14 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    // Method to be overridden (Runtime Polymorphism)
+    public void displayInfo() {
+        System.out.println("Person: " + name + ", Age: " + age);
+    }
+
+    public String getRole() {
+        return "Person";
     }
 }
